22fcedb0e8d1f582a795d8522d8d5f4421dbe10c
unknown <steflowdenver@gmail.com>
2024-03-16 19:45:40 +0530

    second commit

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 let randomnum = Math.round(Math.random() * 10) + 1;
 console.log(randomnum);
-let attemp = 3;
+let attemp = 0;
 
 function guessing() {
     let number = parseInt(document.getElementById("Guess").value);
 
-    attemp--;
-    if (attemp > 0) {
+    attemp++;
+    if (attemp <= 3) {
         if (number == randomnum) {
             document.getElementById("result").innerHTML = ("Congratulation you're won The Game");
         }
@@ -20,7 +20,7 @@ function guessing() {
 
 
     }
-    else {
+    else if (attemp>3) {
         document.getElementById("Guess").disabled = true;
         document.querySelector("button").disabled = true;
         alert("Wrong guess!!!!!! Game Over Try again.......");
